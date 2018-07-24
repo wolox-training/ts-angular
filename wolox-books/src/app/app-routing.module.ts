92dac78ea62039a6e691152245dd478a37b6dfc3
tomas-schmidt <--global>
2018-07-24 10:41:22 -0300

    Add register component

--- wolox-books/src/app/app-routing.module.ts
+++ wolox-books/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '';
+    component: RegisterComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
