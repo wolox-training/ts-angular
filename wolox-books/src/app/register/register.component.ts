78507259f081dd1fcf3bf3a4cb7b7348687d1203
tomas-schmidt <--global>
2018-07-24 13:09:46 -0300

    Add User class and imports

--- wolox-books/src/app/register/register.component.ts
+++ wolox-books/src/app/register/register.component.ts
@@ -1,21 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { User } from './user'
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
+
 export class RegisterComponent implements OnInit {
   rForm: FormGroup;
   post: any;
-  firstName: string;
-  lastName: string;
-  email: string;
-  password: string;
   fieldRequiredAlert: string = "This field is required.";
 
+  private user: User = new User();
+
   constructor(private fb: FormBuilder) {
+
     this.rForm = fb.group({
       'firstName': [null, Validators.required],
       'lastName': [null, Validators.required],
@@ -28,10 +29,9 @@ export class RegisterComponent implements OnInit {
   }
 
   addPost(post) {
-    this.firstName = post.firstName;
-    this.lastName = post.lastName;
-    this.email = post.email;
-    this.password = post.password;
+    this.user.firstName = post.firstName;
+    this.user.lastName = post.lastName;
+    this.user.email = post.email;
+    this.user.password = post.password;
   }
-
 }
---
+++ wolox-books/src/app/register/user.ts
@@ -0,0 +1,8 @@
+export class User  {
+  constructor(firstName, lastName, email, password){
+    this._firstName: = firstName;
+    this._lastName: = lastName;
+    this._email: = email;
+    this._password: = password;
+  }
+}
