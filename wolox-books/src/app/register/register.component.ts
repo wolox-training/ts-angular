f3583333e5fc3e13b0373c4d6db33ee5cb625903
tomas-schmidt <--global>
2018-07-24 11:15:26 -0300

    Add register layout

--- wolox-books/src/app/register/register.component.ts
+++ wolox-books/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  rForm: FormGroup;
+  post: any;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.rForm = fb.group({
+      'firstName': [null, Validators.required],
+      'lastName': [null, Validators.required],
+      'email': [null, Validators.required],
+      'password': [null, Validators.required]
+    });
+
+  }
 
   ngOnInit() {
+  }
+
+  addPost(post) {
+    this.firstName = post.firstName;
+    this.lastName = post.lastName;
+    this.email = post.email;
+    this.password = post.password;
   }
 
 }
