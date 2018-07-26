eff1b415ac3cf2d1ec1b60ed4f1463579ad0d842
tomas-schmidt <--global>
2018-07-26 17:26:08 -0300

    Change post to API

--- wolox-books/src/app/components/user/user.service.ts
+++ wolox-books/src/app/components/user/user.service.ts
@@ -5,12 +5,11 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class UserService {
-  readonly ROOT_URL = 'https://wbooks-api-stage.herokuapp.com/api/v1';
-  newPost: Observable<any>;
+  readonly ROOT_URL = 'https://wbooks-api-stage.herokuapp.com/api/v1/users';
 
   constructor(private http: HttpClient) { }
 
   createUser(user){
-    this.http.post(this.ROOT_URL + "/users", user).subscribe(resp => {console.log(resp);});;
+    this.http.post(this.ROOT_URL, user).subscribe(resp => { console.log(resp) });
   }
 }
