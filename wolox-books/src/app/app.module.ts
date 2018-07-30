3300aee870cfe6421ef856ab111dca3f323e2a57
tomas-schmidt <--global>
2018-07-30 09:26:55 -0300

    Add auth component

--- wolox-books/src/app/app.module.ts
+++ wolox-books/src/app/app.module.ts
@@ -8,12 +8,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './screens/register/register.component';
 import { LoginComponent } from './screens/login/login.component';
+import { AuthComponent } from './screens/auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
