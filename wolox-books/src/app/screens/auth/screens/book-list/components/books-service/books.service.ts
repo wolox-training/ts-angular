b80f4ef36a2fa1561e3d4bdf65b40d0837ff7549
tomas-schmidt <--global>
2018-08-02 10:52:48 -0300

    Add apiUrl enviroment variable, remove nesting on .book, remove getAccessToken function

--- wolox-books/src/app/screens/auth/screens/book-list/components/books-service/books.service.ts
+++ wolox-books/src/app/screens/auth/screens/book-list/components/books-service/books.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LocalStorageService } from '../../../../../../components/local-storage/local-storage.service';
+import { environment } from '../../../../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BooksService {
-  readonly ROOT_URL = 'https://wbooks-api-stage.herokuapp.com/api/v1';
 
   constructor(private http: HttpClient, private localStorage: LocalStorageService) { }
 
   public getBooks(): Observable<Response> {
-    return this.http.get(`${this.ROOT_URL}/books`);
+    return this.http.get(`${environment.apiUrl}/books`);
   }
 }
