604cccc395f34c1da051b9cc59542f8781d66375
tomas-schmidt <--global>
2018-08-06 10:41:54 -0300

    Add getBook function

--- wolox-books/src/app/screens/auth/components/books-service/books.service.ts
+++ wolox-books/src/app/screens/auth/components/books-service/books.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LocalStorageService } from '../../../../components/local-storage/local-storage.service';
 import { environment } from '../../../../../environments/environment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,11 @@ export class BooksService {
 
   constructor(private http: HttpClient, private localStorage: LocalStorageService) { }
 
-  public getBooks(id = ""): Observable<Response> {
+  public getBooks(): Observable<any> {
+    return this.http.get(`${environment.apiUrl}/books`);
+  }
+
+  public getBook(id): Observable<any> {
     return this.http.get(`${environment.apiUrl}/books/${id}`);
   }
 }
