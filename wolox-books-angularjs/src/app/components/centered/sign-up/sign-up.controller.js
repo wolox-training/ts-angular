10778c039e4ba42fe563cc54455ed7634aff0f1a
tomas-schmidt <--global>
2019-03-27 17:38:42 -0300

    remove unused validators

--- wolox-books-angularjs/src/app/components/centered/sign-up/sign-up.controller.js
+++ wolox-books-angularjs/src/app/components/centered/sign-up/sign-up.controller.js
@@ -2,8 +2,8 @@ angular.module('app').controller('SignUpController', ['$scope', '$state',
   function ($scope, $state) {
     this.submit = () => {
       if ($scope.userForm.$valid && $scope.user && $scope.user.password === $scope.user.confirmPassword) {
-				$state.transitionTo('centered.login');
-			}
+        $state.transitionTo('centered.login');
+      }
     }
   }
 ]);
