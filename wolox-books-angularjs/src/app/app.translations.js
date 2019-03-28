4ca8df03f342e0f492920c42993a47dc560a17d8
tomas-schmidt <--global>
2019-03-28 11:30:06 -0300

    add login inputs

--- wolox-books-angularjs/src/app/app.translations.js
+++ wolox-books-angularjs/src/app/app.translations.js
@@ -1,7 +1,9 @@
 angular.module('app').config([
   '$translateProvider',
   function ($translateProvider) {
-    $translateProvider.translations('es', {});
+    $translateProvider.translations('es', {
+      requiredField: 'Campo obligatorio'
+    });
     $translateProvider.preferredLanguage('es');
     $translateProvider.useSanitizeValueStrategy(null);
   }
