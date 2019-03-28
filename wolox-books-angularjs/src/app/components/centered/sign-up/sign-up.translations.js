9eed7a16a86158337e9e86944e23c4a488348696
tomas-schmidt <--global>
2019-03-28 13:37:46 -0300

    change form fields

--- wolox-books-angularjs/src/app/components/centered/sign-up/sign-up.translations.js
+++ wolox-books-angularjs/src/app/components/centered/sign-up/sign-up.translations.js
@@ -5,7 +5,7 @@ angular.module('app').config([
     $translateProvider.translations('es', {
       signUp: {
         signUp: 'Registrarse',
-        name: 'Nombre',
+        firstName: 'Nombre',
         requiredField: 'El campo es obligatorio',
         email: 'Email',
         invalidEmail: 'Ingrese un mail válido',
@@ -13,7 +13,8 @@ angular.module('app').config([
         invalidPassword: 'La contraseña debe tener entre 8 y 53 caracteres y una letra y un numero como mínimo',
         confirmPassword: 'Confirmar contraseña',
         invalidConfirm: 'Las contraseñas no coinciden',
-        submit: 'Aceptar'
+        submit: 'Aceptar',
+        lastName: 'Apellido'
       }
     });
   }
