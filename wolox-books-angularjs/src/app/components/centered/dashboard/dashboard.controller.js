c3f9cb7a93ef34c146dc45e5506cedea91a72454
tomas-schmidt <--global>
2019-04-01 16:12:29 -0300

    remove empty line

--- wolox-books-angularjs/src/app/components/centered/dashboard/dashboard.controller.js
+++ wolox-books-angularjs/src/app/components/centered/dashboard/dashboard.controller.js
@@ -1,6 +1,5 @@
 angular.module('app').controller('DashboardController', [ 'booksService',
   function (booksService) {
-
     booksService.getBooks().then(res => {
       this.books = res.data;
     });
