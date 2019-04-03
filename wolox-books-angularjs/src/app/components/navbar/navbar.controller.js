a452482fd30e8859f218888b911c3d3d0dfe2dfa
tomas-schmidt <--global>
2019-04-03 10:45:58 -0300

    add logout and translates

--- wolox-books-angularjs/src/app/components/navbar/navbar.controller.js
+++ wolox-books-angularjs/src/app/components/navbar/navbar.controller.js
@@ -1,10 +1,21 @@
 angular.module('app').component('navbar', {
   templateUrl: 'app/components/navbar/navbar.html',
-  controller: function NavbarController() {
-    this.showMenu = false;
-
-    this.toggleMenu = () => {
-      this.showMenu = !this.showMenu;
-    }
-  }
+  controller: ['$state', 'localStorageService', NavbarController]
 });
+
+function NavbarController($state, localStorageService) {
+  this.showMenu = false;
+
+  this.toggleMenu = () => {
+    this.showMenu = !this.showMenu;
+  }
+
+  this.goToDashboard = () => {
+    $state.transitionTo('centered.dashboard');
+  }
+
+  this.logOut = () => {
+    localStorageService.remove('accessToken');
+    $state.transitionTo('centered.login');
+  }
+}
