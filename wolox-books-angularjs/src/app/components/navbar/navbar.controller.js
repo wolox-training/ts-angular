f6fa0f5a0329ef1e100ec267b9e693041473911f
tomas-schmidt <--global>
2019-04-03 09:58:41 -0300

    add navbar menu

--- wolox-books-angularjs/src/app/components/navbar/navbar.controller.js
+++ wolox-books-angularjs/src/app/components/navbar/navbar.controller.js
@@ -1,3 +1,10 @@
 angular.module('app').component('navbar', {
   templateUrl: 'app/components/navbar/navbar.html',
+  controller: function NavbarController() {
+    this.showMenu = false;
+
+    this.toggleMenu = () => {
+      this.showMenu = !this.showMenu;
+    }
+  }
 });
