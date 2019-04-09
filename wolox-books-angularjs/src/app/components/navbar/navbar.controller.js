e03f7a224770b149f9dfbb2dbdc25367861d4dc8
tomas-schmidt <--global>
2019-04-09 11:57:28 -0300

    add router ref

--- wolox-books-angularjs/src/app/components/navbar/navbar.controller.js
+++ wolox-books-angularjs/src/app/components/navbar/navbar.controller.js
@@ -10,10 +10,6 @@ function NavbarController($state, localStorageService) {
     this.showMenu = !this.showMenu;
   }
 
-  this.goToDashboard = () => {
-    $state.transitionTo('centered.dashboard');
-  }
-
   this.logOut = () => {
     localStorageService.remove('accessToken');
     $state.transitionTo('centered.login');
