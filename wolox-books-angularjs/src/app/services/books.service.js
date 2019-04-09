03878cde2006b150881b378704320f88d9689e11
tomas-schmidt <--global>
2019-04-09 11:03:14 -0300

    add slice, remove curly braces and returns

--- wolox-books-angularjs/src/app/services/books.service.js
+++ wolox-books-angularjs/src/app/services/books.service.js
@@ -2,12 +2,8 @@ angular.module('app').service('booksService', ['$http', 'configuration', 'localS
   function($http, configuration, localStorageService) {
     const authHeader = { headers: { Authorization: `Bearer ${localStorageService.get('accessToken')}`}};
 
-    this.getBooks = () => {
-      return $http.get(`${configuration.apiUrl}/books`, authHeader);
-    }
+    this.getBooks = () => $http.get(`${configuration.apiUrl}/books`, authHeader);
 
-    this.getBook = id => {
-      return $http.get(`${configuration.apiUrl}/books/${id}`, authHeader);
-    }
+    this.getBook = id => $http.get(`${configuration.apiUrl}/books/${id}`, authHeader);
   }
 ]);
