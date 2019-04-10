57e3cac653eb5c342a86cd153d805292380c43f1
tomas-schmidt <--global>
2019-04-10 13:25:06 -0300

    add notification styles

--- wolox-books-angularjs/src/app/components/navbar/navbar.controller.js
+++ wolox-books-angularjs/src/app/components/navbar/navbar.controller.js
@@ -18,12 +18,46 @@ function NavbarController($state, localStorageService, userService) {
   }
 
   userService.getNotifications().then(res => {
-    this.notifications = res.data;
-    this.notificationsCounter = res.data.length;
+    // this.notifications = res.data;
+    // this.notificationsCounter = res.data.length;
+
+    this.notifications = [
+      {
+        "id": 1,
+        "reason": "book_rent",
+        "from": {
+          "id": 1,
+          "first_name": "Quinn",
+          "last_name": "Thiel",
+          "email": "esteban.pintos+1@wolox.com.ar"
+        },
+        "body": "Content",
+        "action_id": 1,
+        "action_type": "Rent",
+        "read": false,
+        "created_at": "2016-12-28T18:15:19.471Z"
+      },
+      {
+        "id": 2,
+        "reason": "book_rent",
+        "from": {
+          "id": 1,
+          "first_name": "Quinn",
+          "last_name": "Thiel",
+          "email": "esteban.pintos+1@wolox.com.ar"
+        },
+        "body": "Content",
+        "action_id": 1,
+        "action_type": "Comment",
+        "read": true,
+        "created_at": "2016-12-28T18:15:19.482Z"
+      }
+    ];
+    this.notificationsCounter = 2;
   })
 
-  this.showNotifications = () => {
+  this.toggeNotifications = () => {
     if (!this.notificationsCounter) return;
-    this.showingNotifications = true;
+    this.showingNotifications = !this.showingNotifications;
   }
 }
